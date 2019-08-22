7015dbe77e08678f5689e7dcf9ee7df2fbaa5408
Dylan Quesada <dquesada@dylans-mbp.home>
2019-08-21 22:28:38 -0500

    user data

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://user:password1@ds211708.mlab.com:11708/heroku_cwwbw9q5"
+);
 
 // Start the API server
 app.listen(PORT, function() {
